cef9131b9c25fc11d97aa5d22f8cc37569e3f13b
ALBKIN <kinkel.albert@gmail.com>
2023-03-09 17:19:00 +0100

    JediAcademy lekcje 09.03.2023

---
+++ in_2023_03_09/Order.java
@@ -0,0 +1,14 @@
+// LEARN JAVA / CONDITIONALS AND CONTROL FLOWS / If-Then Statement
+
+package in_2023_03_09;
+
+public class Order {
+    public static void main(String[] args) {
+
+        double itemCost = 30.99;
+
+        // Write an if-then statement:
+        if (itemCost > 24.00) System.out.println("High value item!");
+
+    }
+}
---
+++ in_2023_03_09/Order2.java
@@ -0,0 +1,15 @@
+// LEARN JAVA / CONDITIONALS AND CONTROL FLOWS / If-Then-Else
+
+package in_2023_03_09;
+
+public class Order2 {
+    public static void main(String[] args) {
+        boolean isFilled = false;
+
+        if (isFilled) {
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+}

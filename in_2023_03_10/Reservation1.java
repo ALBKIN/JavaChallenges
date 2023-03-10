47d8da4ff52966c482cc0eff0f55f4aa4b9aabf6
ALBKIN <kinkel.albert@gmail.com>
2023-03-10 19:27:06 +0100

    JediAcademy lekcje 09.03.2023

---
+++ in_2023_03_10/Reservation1.java
@@ -0,0 +1,47 @@
+// LEARN JAVA / CONDITIONALS OPERATORS / Conditional-Or: ||
+
+package in_2023_03_10;
+
+public class Reservation1 {
+    int guestCount;
+    int restaurantCapacity;
+    boolean isRestaurantOpen;
+    boolean isConfirmed;
+
+    // constructor method
+    public Reservation1(int count, int capacity, boolean open) {
+        // Write conditional statement below
+        if (count < 1 || count > 8) {
+            System.out.println("Invalid reservation!");
+        }
+        guestCount = count;
+        restaurantCapacity = capacity;
+        isRestaurantOpen = open;
+    }
+
+    // confirm reservation method
+    public void confirmReservation() {
+        if (restaurantCapacity >= guestCount && isRestaurantOpen) {
+            System.out.println("Reservation confirmed");
+            isConfirmed = true;
+        } else {
+            System.out.println("Reservation denied");
+            isConfirmed = false;
+        }
+    }
+
+    // inform user method
+    public void informUser() {
+        System.out.println("Please enjoy your meal");
+    }
+
+    // main method
+    public static void main(String[] args) {
+        Reservation1 partyOfThree = new Reservation1(3, 12, true);
+        Reservation1 partyOfFour = new Reservation1(4, 3, true);
+        partyOfThree.confirmReservation();
+        partyOfThree.informUser();
+        partyOfFour.confirmReservation();
+        partyOfFour.informUser();
+    }
+}

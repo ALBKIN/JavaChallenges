cb44b2a2958edd365439d83da4525e7137d2588c
ALBKIN <kinkel.albert@gmail.com>
2023-03-14 11:56:32 +0100

    JediAcademy lekcje 14.03.2023

---
+++ in_2023_03_14/ToDos.java
@@ -0,0 +1,14 @@
+// LEARN JAVA // ARRAYLISTS / Creating ArrayLists
+
+package in_2023_03_14;
+
+import java.util.ArrayList;
+
+class ToDos {
+
+    public static void main(String[] args) {
+
+        // Create toDoList below:
+        ArrayList<String> toDoList = new ArrayList<String>();
+    }
+}

88fe433b412dae340c401625cf088b63a34ff74d
ALBKIN <kinkel.albert@gmail.com>
2023-03-14 11:01:06 +0100

    JediAcademy lekcje 14.03.2023

---
+++ in_2023_03_14/Newsfeed5.java
@@ -0,0 +1,33 @@
+// LEARN JAVA // ARRAYS / String[] args
+
+package in_2023_03_14;
+
+import java.util.Arrays;
+public class Newsfeed5 {
+    String[] topics;
+
+    // constructor method
+    public Newsfeed5(String[] initialTopics) {
+        topics = initialTopics;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        Newsfeed5 feed;
+        if (args[0].equals("Human")) {
+            String[] humanTopics = {"Politics", "Science", "Sports", "Love"};
+            feed = new Newsfeed5(humanTopics);
+
+        } else if (args[0].equals("Robot")) {
+            String[] robotTopics = {"Oil", "Parts", "Algorithms", "Love"};
+            feed = new Newsfeed5(robotTopics);
+
+        } else {
+            String[] genericTopics = {"Opinion", "Tech", "Science", "Health"};
+            feed = new Newsfeed5(genericTopics);
+        }
+
+        System.out.println("The topics in this feed are:");
+        System.out.println(Arrays.toString(feed.topics));
+    }
+}

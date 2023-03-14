b448761a25636a92a6ad14753ec9b9b7f34e1a01
ALBKIN <kinkel.albert@gmail.com>
2023-03-14 18:00:43 +0100

    JediAcademy lekcje 14.03.2023

---
+++ in_2023_03_14/Coffee.java
@@ -0,0 +1,18 @@
+// LEARN JAVA / LOOPS / Incrementing While Loops
+
+package in_2023_03_14;
+
+class Coffee {
+
+    public static void main(String[] args) {
+
+        // initialize cupsOfCoffee
+        int cupsOfCoffee = 1;
+
+        // add while loop with counter
+        while (cupsOfCoffee <= 100) {
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+            cupsOfCoffee++;
+        }
+    }
+}
---
+++ in_2023_03_14/Coffee1.java
@@ -0,0 +1,13 @@
+// LEARN JAVA / LOOPS / Using For Loops
+
+package in_2023_03_14;
+
+public class Coffee1 {
+
+    public static void main(String[] args) {
+
+        for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+        }
+    }
+}

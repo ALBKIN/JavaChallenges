dc4ca681d9367e784b2ad4bc31d20f02401c04cd
ALBKIN <kinkel.albert@gmail.com>
2023-03-15 10:05:34 +0100

    JediAcademy lekcje 15.03.2023

---
+++ in_2023_03_15/FizzBuzz.java
@@ -0,0 +1,23 @@
+// LEARN JAVA / FIZZBUZZ QUESTION
+
+package in_2023_03_15;
+
+class FizzBuzz {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}

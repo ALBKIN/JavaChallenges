5092cb789f2d421572a76dcb608ef77bb35a76ac
ALBKIN <kinkel.albert@gmail.com>
2023-03-23 14:46:12 +0100

    JediAcademy lekcje 23.03.2023

---
+++ in_2023_03_23/ImageProcessing.java
@@ -0,0 +1,264 @@
+// LEARN JAVA / 2D ARRAYS: JAVA / PROJECT / 2D Arrays: Image Manipulation Project
+
+package in_2023_03_23;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Random;
+import javax.imageio.ImageIO;
+
+public class ImageProcessing {
+    ublic
+
+    static void main(String[] args) {
+        // The provided images are apple.jpg, flower.jpg, and kitten.jpg
+        //	int[][] imageData = imgToTwoD("./kitten.jpg");
+        // Or load your own image using a URL!
+        int[][] imageData = imgToTwoD("https://i.im.ge/2023/03/23/DeIDQc.keep-scrolling.jpg");
+
+        // https://content.codecademy.com/projects/project_thumbnails/phaser/bug-dodger.png");
+        //viewImageData(imageData);
+        int[][] trimmed = trimBorders(imageData, 60);
+        twoDToImage(trimmed, "./trimmed_godzilla.jpg");
+        // int[][] allFilters = stretchHorizontally(shrinkVertically(colorFilter(negativeColor(trimBorders(invertImage(imageData), 50)), 200, 20, 40)));
+        // Painting with pixels
+
+        // MAKE NEGATIVE MUAHAHA
+        int[][] negative = negativeColor(imageData);
+        twoDToImage(negative, "./negative_godzilla.jpg");
+
+        // MAKE STRETCHED MUAHAHAHAHHA
+        int[][] stretched = stretchHorizontally(imageData);
+        twoDToImage(stretched, "./stretched_godzilla.jpg");
+
+        // MAKE SHRINK MUAHAHAAHAHAHHAH
+        int[][] shrinked = shrinkVertically(imageData);
+        twoDToImage(shrinked, "./shrinked_godzilla.jpg");
+
+        // MAKE INVERT MUAHHUHAUAHAUHAUHA
+        int[][] inverted = invertImage(imageData);
+        twoDToImage(inverted, "./inverted_godzilla.jpg");
+
+        // MAKE FILTER BUAHOHOOHOHOONIOHNIOHNIOH
+        int[][] filtered = colorFilter(imageData, -50, 35, -17);
+        twoDToImage(filtered, "./filtered_godzilla.jpg");
+
+
+    }
+
+    // Image Processing Methods
+    public static int[][] trimBorders(int[][] imageTwoD, int pixelCount) {
+        // Example Method
+        if (imageTwoD.length > pixelCount * 2 && imageTwoD[0].length > pixelCount * 2) {
+            int[][] trimmedImg = new int[imageTwoD.length - pixelCount * 2][imageTwoD[0].length - pixelCount * 2];
+            for (int i = 0; i < trimmedImg.length; i++) {
+                for (int j = 0; j < trimmedImg[i].length; j++) {
+                    trimmedImg[i][j] = imageTwoD[i + pixelCount][j + pixelCount];
+                }
+            }
+            return trimmedImg;
+        } else {
+            System.out.println("Cannot trim that many pixels from the given image.");
+            return imageTwoD;
+        }
+    }
+
+    public static int[][] negativeColor(int[][] imageTwoD) {
+        // TODO: Fill in the code for this method
+        int[][] negativeImg = new int[imageTwoD.length][imageTwoD[0].length];
+        for (int i = 0; i < imageTwoD.length; i++) {
+            for (int j = 0; j < imageTwoD[i].length; j++) {
+                int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
+                rgba[0] = 255 - rgba[0];
+                rgba[1] = 255 - rgba[1];
+                rgba[2] = 255 - rgba[2];
+                negativeImg[i][j] = getColorIntValFromRGBA(rgba);
+            }
+        }
+        return negativeImg;
+    }
+
+    public static int[][] stretchHorizontally(int[][] imageTwoD) {
+        // TODO: Fill in the code for this method
+        int[][] stretchedImg = new int[imageTwoD.length][imageTwoD[0].length * 2];
+        int it = 0;
+        for (int rowIndex = 0; rowIndex < imageTwoD.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < imageTwoD[rowIndex].length; columnIndex++) {
+                it = columnIndex * 2;
+                stretchedImg[rowIndex][it] = imageTwoD[rowIndex][columnIndex];
+                stretchedImg[rowIndex][it + 1] = imageTwoD[rowIndex][columnIndex];
+            }
+        }
+        return stretchedImg;
+    }
+
+    public static int[][] shrinkVertically(int[][] imageTwoD) {
+        // TODO: Fill in the code for this method
+        int[][] shrinkedImg = new int[imageTwoD.length / 2][imageTwoD[0].length];
+        for (int columnIndx = 0; columnIndx < imageTwoD[0].length; columnIndx++) {
+            for (int rowIndx = 0; rowIndx < imageTwoD.length - 1; rowIndx += 2) {
+                shrinkedImg[rowIndx / 2][columnIndx] = imageTwoD[rowIndx][columnIndx];
+            }
+        }
+        return shrinkedImg;
+    }
+
+    public static int[][] invertImage(int[][] imageTwoD) {
+        // TODO: Fill in the code for this method
+        int[][] invertedImg = new int[imageTwoD.length][imageTwoD[0].length];
+        for (int i = 0; i < imageTwoD.length; i++) {
+            for (int j = 0; j < imageTwoD[i].length; j++) {
+                invertedImg[i][j] = imageTwoD[(imageTwoD.length - 1) - i][(imageTwoD[i].length - 1) - j];
+            }
+        }
+        return invertedImg;
+    }
+
+    public static int[][] colorFilter(int[][] imageTwoD, int redChangeValue, int greenChangeValue, int blueChangeValue) {
+        // TODO: Fill in the code for this method
+        int[][] filteredImg = new int[imageTwoD.length][imageTwoD[0].length];
+        for (int i = 0; i < imageTwoD.length; i++) {
+            for (int j = 0; j < imageTwoD[i].length; j++) {
+                int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
+                int newRed = rgba[0] + redChangeValue;
+                int newGreen = rgba[1] + greenChangeValue;
+                int newBlue = rgba[2] + blueChangeValue;
+        /* SKRÓCONY IF NIE DZIAŁA
+        if (newRed < 0 || newGreen < 0 || newBlue < 0) {
+        newRed = 0 || newGreen = 0 || newBlue = 0;
+        } else if (newRed > 255 || newGreen > 255 || newBlue >255) {
+        newRed = 255 || newGreen = 255 || newBlue = 255;
+        }
+        WIĘC PISZEMY DOOKOŁA
+        */
+                if (newRed < 0) {
+                    newRed = 0;
+                } else if (newRed > 255) {
+                    newRed = 255;
+                }
+                if (newBlue < 0) {
+                    newBlue = 0;
+                } else if (newBlue > 255) {
+                    newBlue = 255;
+                }
+                if (newGreen < 0) {
+                    newGreen = 0;
+                } else if (newGreen > 255) {
+                    newGreen = 255;
+                }
+
+                rgba[0] = newRed;
+                rgba[1] = newGreen;
+                rgba[2] = newBlue;
+
+                filteredImg[i][j] = getColorIntValFromRGBA(rgba);
+            }
+        }
+        return filteredImg;
+    }
+
+    // Painting Methods
+    public static int[][] paintRandomImage(int[][] canvas) {
+        // TODO: Fill in the code for this method
+        return null;
+    }
+
+    public static int[][] paintRectangle(int[][] canvas, int width, int height, int rowPosition, int colPosition, int color) {
+        // TODO: Fill in the code for this method
+        return null;
+    }
+
+    public static int[][] generateRectangles(int[][] canvas, int numRectangles) {
+        // TODO: Fill in the code for this method
+        return null;
+    }
+
+    // Utility Methods
+    public static int[][] imgToTwoD(String inputFileOrLink) {
+        try {
+            BufferedImage image = null;
+            if (inputFileOrLink.substring(0, 4).toLowerCase().equals("http")) {
+                URL imageUrl = new URL(inputFileOrLink);
+                image = ImageIO.read(imageUrl);
+                if (image == null) {
+                    System.out.println("Failed to get image from provided URL.");
+                }
+            } else {
+                image = ImageIO.read(new File(inputFileOrLink));
+            }
+            int imgRows = image.getHeight();
+            int imgCols = image.getWidth();
+            int[][] pixelData = new int[imgRows][imgCols];
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    pixelData[i][j] = image.getRGB(j, i);
+                }
+            }
+            return pixelData;
+        } catch (Exception e) {
+            System.out.println("Failed to load image: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    public static void twoDToImage(int[][] imgData, String fileName) {
+        try {
+            int imgRows = imgData.length;
+            int imgCols = imgData[0].length;
+            BufferedImage result = new BufferedImage(imgCols, imgRows, BufferedImage.TYPE_INT_RGB);
+            for (int i = 0; i < imgRows; i++) {
+                for (int j = 0; j < imgCols; j++) {
+                    result.setRGB(j, i, imgData[i][j]);
+                }
+            }
+            File output = new File(fileName);
+            ImageIO.write(result, "jpg", output);
+        } catch (Exception e) {
+            System.out.println("Failed to save image: " + e.getLocalizedMessage());
+        }
+    }
+
+    public static int[] getRGBAFromPixel(int pixelColorValue) {
+        Color pixelColor = new Color(pixelColorValue);
+        return new int[]{pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha()};
+    }
+
+    public static int getColorIntValFromRGBA(int[] colorData) {
+        if (colorData.length == 4) {
+            Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
+            return color.getRGB();
+        } else {
+            System.out.println("Incorrect number of elements in RGBA array.");
+            return -1;
+        }
+    }
+
+    public static void viewImageData(int[][] imageTwoD) {
+        if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
+            int[][] rawPixels = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rawPixels[i][j] = imageTwoD[i][j];
+                }
+            }
+            System.out.println("Raw pixel data from the top left corner.");
+            System.out.print(Arrays.deepToString(rawPixels).replace("],", "],\n") + "\n");
+            int[][][] rgbPixels = new int[3][3][4];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    rgbPixels[i][j] = getRGBAFromPixel(imageTwoD[i][j]);
+                }
+            }
+            System.out.println();
+            System.out.println("Extracted RGBA pixel data from top the left corner.");
+            for (int[][] row : rgbPixels) {
+                System.out.print(Arrays.deepToString(row) + System.lineSeparator());
+            }
+        } else {
+            System.out.println("The image is not large enough to extract 9 pixels from the top left corner");
+        }
+    }
+}

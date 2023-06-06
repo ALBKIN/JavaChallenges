ee1218d75a1076adaa4636c02a0073ff96862e32
ALBKIN <kinkel.albert@gmail.com>
2023-06-06 22:04:58 +0200

    06.06.2023

---
+++ in_2023_06_06/plants/PlantsApplication.java
@@ -0,0 +1,13 @@
+package in_2023_06_06.plants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantsApplication.class, args);
+    }
+
+}
---
+++ in_2023_06_06/plants/repositories/PlantRepository.java
@@ -0,0 +1,15 @@
+package in_2023_06_06.plants.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import in_2023_06_06.plants.entities.Plant;
+
+public interface PlantRepository extends CrudRepository<Plant, Integer> {
+    List<Plant> findByHasFruitTrue();
+    List<Plant> findByHasFruitFalse();
+    List<Plant> findByQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
+}

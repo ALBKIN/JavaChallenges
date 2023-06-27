4abd5e537d046e2e48b04a7c6b15b06e670f30a6
ALBKIN <kinkel.albert@gmail.com>
2023-06-27 16:14:55 +0200

    27.06.2023

---
+++ in_2023_06_27/fibMemo.java
@@ -0,0 +1,27 @@
+package in_2023_06_27;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fibMemo {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        System.out.println(fibMemo(10, map));
+
+        map = new HashMap<Integer, Integer>();
+        System.out.println(fibMemo(20, map));
+    }
+
+        public static int fibMemo(int n, Map<Integer, Integer> map) {
+            if (n == 0 || n == 1) {
+                return n;
+            } else if (map.containsKey(n)) {
+                return map.get(n);
+            } else {
+                int calculatedFib = fibMemo(n-1, map) + fibMemo(n-2, map);
+                map.put(n, calculatedFib);
+                return calculatedFib;
+            }
+        }
+    }

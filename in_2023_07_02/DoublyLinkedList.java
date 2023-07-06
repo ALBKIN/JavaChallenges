6a5f4df25ee20eebcc9c99425ded4a59112fb856
ALBKIN <kinkel.albert@gmail.com>
2023-07-06 13:58:28 +0200

    06.07.2023

---
+++ in_2023_07_02/DoublyLinkedList.java
@@ -0,0 +1,8 @@
+package in_2023_07_02;
+
+import in_2023_07_01.Node;
+
+public class DoublyLinkedList {
+    public Node head;
+    public Node tail;
+}
---
+++ in_2023_07_02/Node.java
@@ -0,0 +1,9 @@
+package in_2023_07_02;
+
+public class Node {
+    public String data;
+    public Node next;
+    public Node previous;
+
+    public Node(String data);
+}

2257ea99711e6b1fa84bb25a937e9ba0c8187fc8
ALBKIN <kinkel.albert@gmail.com>
2023-07-13 20:29:07 +0200

    Java Challanges 13.07.2023

--- in_2023_07_13/BinarySearchTree.java
+++ in_2023_07_13/BinarySearchTree.java
@@ -7,6 +7,7 @@ public class BinarySearchTree {
     public BinarySearchTree left;
     public BinarySearchTree right;
 
+    // Constructor
     public BinarySearchTree(int value, int depth) {
         this.value = value;
         this.depth = depth;
@@ -14,6 +15,7 @@ public class BinarySearchTree {
         this.right = null;
     }
 
+    // Constructor for root Node of BST
     public BinarySearchTree(int value) {
         this.value = value;
         this.depth = 1;
@@ -21,12 +23,35 @@ public class BinarySearchTree {
         this.right = null;
     }
 
+    // Insert method
+    public void insert(int value) {
+        if (value < this.value) {
+            if (this.left == null) {
+                this.left = new BinarySearchTree(value, this.depth + 1);
+                System.out.printf("Tree node %d added to the left of %d at depth %d \n", value, this.value, this.depth + 1);
+            } else {
+                this.left.insert(value);
+            }
+        } else if (value > this.value) {
+            if (this.right == null) {
+                this.right = new BinarySearchTree(value, this.depth + 1);
+                System.out.printf("Tree node %d added to the right of %d at depth %d \n", value, this.value, this.depth + 1);
+            } else {
+                this.right.insert(value);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // Create a new BST:
         BinarySearchTree bt = new BinarySearchTree(15);
-
         // Print bt's value below:
         System.out.println(bt.value);
 
+        BinarySearchTree root = new BinarySearchTree(100);
+        root.insert(50);
+        root.insert(125);
+        root.insert(75);
+        root.insert(25);
     }
 }

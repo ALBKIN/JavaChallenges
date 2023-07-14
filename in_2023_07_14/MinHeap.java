12542607343f33595f2502e8f3de8165ad0296e7
ALBKIN <kinkel.albert@gmail.com>
2023-07-14 12:40:56 +0200

    Java Challanges 14.07.2023

--- in_2023_07_14/MinHeap.java
+++ in_2023_07_14/MinHeap.java
@@ -13,8 +13,21 @@ public class MinHeap {
         this.heap = new ArrayList<>();
         this.heap.add(null);
         this.size = 0;
-
     }
+
+    // Define add()
+    public void add(int value) {
+        heap.add(value);
+        size++;
+        System.out.println("Adding " + value);
+        System.out.println(this.heap);
+        this.bubbleUp();
+    }
+
+    private void bubbleUp() {
+        System.out.println("Restoring heap condition...");
+    }
+
 
     public static void main(String[] args) {
         //Instantiate a MinHeap, assign to minHeap
@@ -23,5 +36,8 @@ public class MinHeap {
         //Display contents of minHeap
         System.out.println(minHeap.heap);
         System.out.println(minHeap.size);
+
+        // Add 42 to minHeap using add() below
+        minHeap.add(42);
     }
 }

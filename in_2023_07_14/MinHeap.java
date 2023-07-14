d829ac7f4d0314be3549c4327680c691f50efbe1
ALBKIN <kinkel.albert@gmail.com>
2023-07-14 12:31:53 +0200

    Java Challanges 14.07.2023

--- in_2023_07_14/MinHeap.java
+++ in_2023_07_14/MinHeap.java
@@ -1,4 +1,27 @@
 package in_2023_07_14;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MinHeap {
+
+    public ArrayList<Integer> heap;
+    public int size;
+
+    // Define MinHeap constructor
+    public MinHeap() {
+        this.heap = new ArrayList<>();
+        this.heap.add(null);
+        this.size = 0;
+
+    }
+
+    public static void main(String[] args) {
+        //Instantiate a MinHeap, assign to minHeap
+        MinHeap minHeap = new MinHeap();
+
+        //Display contents of minHeap
+        System.out.println(minHeap.heap);
+        System.out.println(minHeap.size);
+    }
 }

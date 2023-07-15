85a6a7d40101372a65991cefca576da03a238d0a
ALBKIN <kinkel.albert@gmail.com>
2023-07-15 12:24:13 +0200

    Java Challanges 15.07.2023 MergeSort Algorithm

--- in_2023_07_15/MergeSort.java
+++ in_2023_07_15/MergeSort.java
@@ -17,15 +17,35 @@ public class MergeSort {
         int midIndex = Math.floorDiv(length, 2);
         int[] leftArray = Arrays.copyOfRange(arr, 0, midIndex);
         int[] rightArray = Arrays.copyOfRange(arr, midIndex, length);
-        this.sort(leftArray);
-        this.sort(rightArray);
-        return null;
+
+        return merge(sort(leftArray), sort(rightArray));
     }
 
-    public static void main(String[] args) {
-        int[] inputArr = {3, 5 , 2 ,90 , 4 , 7};
+    public int[] merge(int left[], int[] right) {
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        int[] merged = new int[left.length + right.length];
+        int leftPos = 0;
+        int rightPos = 0;
+        int curIndex = 0;
+        while (left.length > leftPos && right.length > rightPos) {
+            if (left[leftPos] < right[rightPos]) {
+                merged[curIndex] = left[leftPos];
+                leftPos++;
+            } else {
+                merged[curIndex] = right[rightPos];
+                rightPos++;
+            }
+            curIndex++;
+        }
+        System.arraycopy(left, leftPos, merged, curIndex, left.length - leftPos);
+        System.arraycopy(right, rightPos, merged, curIndex, right.length - rightPos);
+        return merged;
+    }
+
+    public static void main (String[] args) {
+        int[] inputArr = {3, 5, 2, 90, 4, 7};
         MergeSort sorter = new MergeSort();
         System.out.println(Arrays.toString(sorter.sort(inputArr)));
-
     }
 }

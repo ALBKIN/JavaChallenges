6396190e4f7079b6f30578117700fef1a479b397
ALBKIN <kinkel.albert@gmail.com>
2023-07-16 00:02:39 +0200

    Java Challanges 16.07.2023 QuickSort Algorithm

---
+++ in_2023_07_16/QuickSort.java
@@ -0,0 +1,74 @@
+package in_2023_07_16;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static in_2023_07_15.Swap.swap;
+
+public class QuickSort {
+
+    public int partition(int[] arr, int leftPointer, int rightPointer) {
+        System.out.println("Current array / subarray being partitioned: " + Arrays.toString(Arrays.copyOfRange(arr, leftPointer, rightPointer + 1)));
+
+        int pivot = arr[Math.floorDiv((leftPointer + rightPointer), 2)];
+        System.out.println("The pivot value is: " + pivot);
+
+        while (leftPointer < rightPointer) {
+            while (arr[leftPointer] < pivot) {
+                leftPointer++;
+                System.out.println("Incrementing left pointer to " + arr[leftPointer]);
+            }
+            while (arr[rightPointer] > pivot) {
+                rightPointer--;
+                System.out.println("Decrementing right pointer to " + arr[rightPointer]);
+            }
+            if (leftPointer < rightPointer) {
+                System.out.println("Swapping " + arr[leftPointer] + " and " + arr[rightPointer]);
+                swap(arr, leftPointer, rightPointer);
+                /*leftPointer++;
+                rightPointer--;*/
+            }
+        }
+        return leftPointer;
+    }
+
+    public int[] quickSort(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        return quickSort(arr, 0, arr.length -1);
+    }
+    public int[] quickSort(int[] arr, int start, int end) {
+        if (end > start) {
+            // Call partition
+            int partition = partition(arr, start, end);
+            // Recursive code on two sides of partition
+            if (start < partition) {
+                quickSort(arr, start, partition);
+            }
+            if (partition + 1 < end) {
+                quickSort(arr, partition + 1, end);
+            }
+        }
+        return arr; // Base case code
+    }
+
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+
+        Random r = new Random();
+        int[] unsorted = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+            unsorted[i] = r.nextInt(50);
+        }
+
+        // int pivotPosition = qs.partition(testArr, 0, testArr.length-1);
+        // System.out.println("Pivot position: " + pivotPosition);
+
+        System.out.println("Sorting the array " + Arrays.toString(unsorted));
+        qs.quickSort(unsorted);
+        System.out.println("Array after sorting: " + Arrays.toString(unsorted));
+
+    }
+}

7e5eddd6e78000e323e85d68a8aa25746242d98d
ALBKIN <kinkel.albert@gmail.com>
2023-07-15 10:47:40 +0200

    Java Challanges 15.07.2023 BubbleSort Algorithm

---
+++ in_2023_07_15/Swap.java
@@ -0,0 +1,11 @@
+package in_2023_07_15;
+
+public class Swap {
+    public static void swap(int[] arr, int indexOne, int indexTwo) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] example = {8, 12, 20, 14, 53, 7};
+    }
+}

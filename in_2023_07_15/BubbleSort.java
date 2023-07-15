1b921d1d6b6c5fba800ce3a7e8a47c568babb960
ALBKIN <kinkel.albert@gmail.com>
2023-07-15 11:05:37 +0200

    Java Challanges 15.07.2023 BubbleSort Algorithm

--- in_2023_07_15/BubbleSort.java
+++ in_2023_07_15/BubbleSort.java
@@ -4,7 +4,11 @@ package in_2023_07_15;
 This class contains the code implementation for Bubble Sort algorithm
  */
 
+import java.sql.Array;
 import java.util.Arrays;
+import java.util.Random;
+
+import static in_2023_07_15.Swap.swap;
 
 public class BubbleSort {
 
@@ -13,10 +17,10 @@ public class BubbleSort {
         while (swapping) {
             swapping = false;
             for (int i = 0; i < input.length - 1; i++) {
-                if (input[i] >= input[i+1]) {
-                    int temp = input[i];
-                    input[i] = input[i+1];
-                    input[i+1] = temp;
+                if (input[i] > input[i+1]) {
+                    System.out.println(String.format("Swapping pair %d, %d", input[i], input[i+1]));
+                    Swap.swap(input, i, i+1);
+                    System.out.println(Arrays.toString(input));
                     swapping = true;
                 }
             }
@@ -25,9 +29,14 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] example = {8 ,12, 20, 14, 53, 7};
+
+        Random r = new Random();
+        int[] example = new int[5];
+        for (int i = 0; i < 5; i++) {
+            example[i] = r.nextInt(50);
+        }
 
         System.out.println("Original unsorted input array: \n" + Arrays.toString(example) +"\n");
-        System.out.println("Using Bubble Sort algorithm: ...... \n" + Arrays.toString(bubbleSort(example)));
+        System.out.println("\nResult of using Bubble Sort algorithm: ...... \n" + Arrays.toString(bubbleSort(example)));
     }
 }

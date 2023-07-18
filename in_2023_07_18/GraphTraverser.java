a45e58188d62f78a0401b4e3db4908e8804f5e7f
ALBKIN <kinkel.albert@gmail.com>
2023-07-18 14:35:19 +0200

    Java Challanges 18.07.2023 Graph DFT Depth First Traversal (ONE PATH)

--- in_2023_07_18/GraphTraverser.java
+++ in_2023_07_18/GraphTraverser.java
@@ -7,22 +7,24 @@ import java.util.ArrayList;
 
 public class GraphTraverser {
 
-    public static void depthFirstTraversal(Vertex start) {
+    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices) {
         System.out.println(start.getData());
         if (start.getEdges().size() > 0) {
             Vertex neighbor = start.getEdges().get(0).getEnd();
 
-            GraphTraverser.depthFirstTraversal(neighbor);
+            if (!visitedVertices.contains(neighbor)) {
+                visitedVertices.add(neighbor);
+                GraphTraverser.depthFirstTraversal(neighbor, visitedVertices);
+            }
         }
-
-
     }
+
     public static void main(String[] args) {
         TestGraph test = new TestGraph();
         Vertex startingVertex = test.getStartingVertex();
         ArrayList<Vertex> visitedVertices = new ArrayList<Vertex>();
         visitedVertices.add(startingVertex);
 
-        GraphTraverser.depthFirstTraversal(startingVertex);
+        GraphTraverser.depthFirstTraversal(startingVertex, visitedVertices);
     }
 }

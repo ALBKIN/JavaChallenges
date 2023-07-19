14d412f1a4129753e66fc9c0e7e577beeb4e7ca3
ALBKIN <kinkel.albert@gmail.com>
2023-07-19 18:50:32 +0200

    Java Challanges 18.07.2023 Dijkstra Algorithm cont.

---
+++ in_2023_07_19/QueueObject.java
@@ -0,0 +1,25 @@
+package in_2023_07_19;
+
+import in_2023_07_16.Vertex;
+import in_2023_07_18.Queue;
+
+public class QueueObject implements Comparable<QueueObject> {
+    public Vertex vertex;
+    public int priority;
+
+    public QueueObject(Vertex v, int p) {
+        this.vertex = v;
+        this.priority = p;
+    }
+
+    @Override
+    public int compareTo(QueueObject o) {
+        if (this.priority == o.priority) {
+            return 0;
+        } else if (this.priority > o.priority) {
+            return 1;
+        } else if (this.priority < o.priority) {
+            return -1;
+        }
+    }
+}

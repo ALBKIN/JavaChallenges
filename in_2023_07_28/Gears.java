4d99d15b5edfc02b2a6d54a833ae951b8d20b02c
ALBKIN <kinkel.albert@gmail.com>
2023-07-28 11:31:04 +0200

    Java Challanges 28.07.2023 Serialization (Custom Serialization)

---
+++ in_2023_07_28/Gears.java
@@ -0,0 +1,16 @@
+package in_2023_07_28;
+
+public class Gears {
+
+    private int gearsNo;
+    private boolean automatic;
+
+    private Gears(int gearsNo, boolean automatic) {
+        this.gearsNo = gearsNo;
+        this.automatic = automatic;
+    }
+
+    public String toString() {
+        return String.format("Gears number is:%s, Has automatic gearshift: %b", this.gearsNo, this.automatic);
+    }
+}

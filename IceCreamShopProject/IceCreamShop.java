85ae79bf5bc77faf7b07663a9b6d9ed77fb619c2
ALBKIN <kinkel.albert@gmail.com>
2023-09-04 22:29:48 +0200

    Ice Cream Shop Project Nested classes practice

--- IceCreamShopProject/IceCreamShop.java
+++ IceCreamShopProject/IceCreamShop.java
@@ -13,11 +13,11 @@ class ShopItems {
         public void increseCount(int type, int count) {
             if (type == 1) {
                 iceCreamCount += count;
-                ShopItems.this.totalCount += iceCreamCount;
             } else if (type == 2) {
                 shakeCount += count;
-                ShopItems.this.totalCount += shakeCount;
             }
+            totalCount = iceCreamCount + shakeCount;
+            ShopItems.this.totalCount += shakeCount;
         }
     }
 
@@ -29,40 +29,46 @@ class ShopItems {
         public void increseCount(int type, int count) {
             if (type == 3) {
                 smoothieCount += count;
-                ShopItems.this.totalCount += smoothieCount;
             } else if (type == 4) {
                 slushieCount += count;
-                ShopItems.this.totalCount += slushieCount;
             }
+            totalCount = smoothieCount + slushieCount;
+            ShopItems.this.totalCount += slushieCount;
         }
     }
-
-
-
 }
 
 public class IceCreamShop {
 
     public static void main(String[] args) {
+        ShopItems items = new ShopItems();
+        ShopItems.Vegan vegan = items.new Vegan();
+        ShopItems.NonVegan nonVegan = items.new NonVegan();
+
         int orderType = 0;
         Scanner input = new Scanner(System.in);
         int itemCount = 0;
         String itemName;
 
-        System.out.println("Hello! Welcome to the ice cream shop. We are ready to take your order. For ice-cream type 1, " +
-                "for shakes type 2, for smoothies type 3, and for slushies type 4. If you are done ordering type 0.");
+        System.out.println("Hello! Welcome to the ice cream shop. We are ready to take your order. \nFor ice-cream type 1, " +
+                "\nfor shakes type 2, \nfor smoothies type 3, and \nfor slushies type 4. \n\nIf you are done ordering type 0.");
+        orderType = input.nextInt();
 
         while (orderType != 0) {
             System.out.println("How many items of this item would you like to order?");
             itemCount = input.nextInt();
             if (orderType == 1) {
                 itemName = "ice cream";
+                nonVegan.increseCount(1, itemCount);
             } else if (orderType == 2) {
                 itemName = "shake";
+                nonVegan.increseCount(2, itemCount);
             } else if (orderType == 3) {
                 itemName = "smoothie";
+                vegan.increseCount(3, itemCount);
             } else if (orderType == 4) {
                 itemName = "slushie";
+                vegan.increseCount(4, itemCount);
             } else {
                 break;
             }
@@ -70,14 +76,12 @@ public class IceCreamShop {
             System.out.println("Adding " + itemCount + " of the item " + itemName + " to your order!");
 
             System.out.println("Type a number for the next item in your order:\n1: Ice Cream\n2: Shake\n3:" +
-                    "Smoothie\n4: Slushie\n");
+                    " Smoothie\n4: Slushie\n0: GO TO COUNTER");
             orderType = input.nextInt();
         }
 
-        ShopItems items = new ShopItems();
-        ShopItems.Vegan vegan = items.new Vegan();
-        ShopItems.NonVegan nonVegan = items.new NonVegan();
-
+        System.out.println("Thank you for your order! Today we have sold " + items.totalCount + " orders of sweetness!");
+        System.out.println(nonVegan.totalCount + " items have been ice cream and shakes.");
+        System.out.println(vegan.totalCount + " items have been smoothies and slushies.");
     }
-
 }

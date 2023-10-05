21e471a46aeeb88f3982daaf5379d32b3258f64a
ALBKIN <kinkel.albert@gmail.com>
2023-10-05 13:31:34 +0200

    LeetCode Java Challenge 21: MergeSortedLists

---
+++ in_2023_10_05/MergeSortedLists.java
@@ -0,0 +1,80 @@
+/*
+  LEETCODE JAVA CHALLANGES: 21. Merge Two Sorted Lists
+  You are given the heads of two sorted linked lists list1 and list2.
+  Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+  Return the head of the merged linked list.
+  Example 1:
+  Input: list1 = [1,2,4], list2 = [1,3,4]
+  Output: [1,1,2,3,4,4]
+
+  Example 2:
+  Input: list1 = [], list2 = []
+  Output: []
+
+  Example 3:
+  Input: list1 = [], list2 = [0]
+  Output: [0]
+
+ Constraints:
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+*/
+
+package in_2023_10_05;
+
+import in_2023_08_31.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSortedLists {
+
+    // Definition for singly-linked list
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // base cases
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        // recursive merging
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSortedLists merger = new MergeSortedLists();
+
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode mergedList = merger.mergeTwoLists(list1, list2);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
